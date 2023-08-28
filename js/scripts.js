47d4d770b1443f52572bbe9c89782602dc7bb85e
Shevin <shevinfernando123@gmail.com>
2023-08-28 17:17:50 +1000

    Commit 1

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+const clickMe = () => {
+    alert("Thanks for clicking me. Hope you have a nice day!")
+}
+const submitForm = () => {
+    let formData = {};
+    formData.title = $('#title').val(); 
+    formData.image = $('#image_link').val(); 
+    formData.description = $('#description').val(); 
+    formData.link = $('#link').val();
+    console.log("Form Data Submitted: ", formData);
+    postDog(formData)
+}
+
+function postDog(dog){
+    $.ajax({
+        url:'/api/dog',
+        type:'POST',
+        data:dog,
+        success: (result)=>{
+            if(result.statusCode === 201){
+                alert('Animal created')
+            }
+        }
+    })
+}
+
+function getAllDogs(){
+    $.get('/api/dog',(result)=>{
+        if(result.statusCode === 200){
+            //Idk
+        }
+    })
+}
+
+$(document).ready(function () {
+    $('.materialboxed').materialbox(); 
+    $('#formSubmit').click(() => {
+        submitForm();
+    })
+    $('.modal').modal();
+    getAllDogs();
+});
+
